bfb3b22250ba2c2abeef26b3dbed67250cdd2e28
seoyeong <syoung622@naver.com>
2022-12-08 22:22:03 +0900

    feat: cors 추가

---
+++ mysnowball/src/main/java/com/lgu/mysnowball/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.lgu.mysnowball.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "http://localhost:8080", "https://ec2-3-114-67-166.ap-northeast-1.compute.amazonaws.com")
+            .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS", "PATCH")
+            .allowedHeaders("Origin", "Content-Type", "Authorization", "Bearer", "refreshToken")
+            .allowCredentials(true);
+    }
+}
