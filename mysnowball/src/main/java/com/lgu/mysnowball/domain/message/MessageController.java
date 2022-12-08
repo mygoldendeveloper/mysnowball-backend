e3790a734f48bee7dfcb26d257bcd0e918a1638e
sanhajung <sanhajung3@gmail.com>
2022-12-08 19:56:33 +0900

    faet: 친구 목록 받아오기 테스트

--- mysnowball/src/main/java/com/lgu/mysnowball/domain/message/MessageController.java
+++ mysnowball/src/main/java/com/lgu/mysnowball/domain/message/MessageController.java
@@ -1,16 +1,28 @@
 package com.lgu.mysnowball.domain.message;
 
+import com.lgu.mysnowball.kakao.KakaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/kakao/message")
 public class MessageController {
 
+    @Autowired
+    private MessageService messageService;
+
     @PostMapping()
     public void saveMessage(){
 
+    }
+
+    @GetMapping(value = "/frineds")
+    public void login() {
+        String access_Token = messageService.getFriends();
     }
 
 }
