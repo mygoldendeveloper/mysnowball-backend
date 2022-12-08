9e10b6fbff753f82420cf79243365e8656826ee4
seoyeong <syoung622@naver.com>
2022-12-09 07:57:16 +0900

    feat: cors 추가

--- mysnowball/src/main/java/com/lgu/mysnowball/config/WebConfig.java
+++ mysnowball/src/main/java/com/lgu/mysnowball/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "http://localhost:8080", "https://ec2-3-114-67-166.ap-northeast-1.compute.amazonaws.com", "http://3.114.67.166",
-                "https://mysnowball.me")
+            .allowedOrigins("http://localhost:3000", "http://localhost:8080", "https://ec2-3-114-67-166.ap-northeast-1.compute.amazonaws.com", "http://3.114.67.166:8080",
+                "https://www,mysnowball.me", "https://mysnowball.me")
             .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS", "PATCH")
             .allowedHeaders("Origin", "Content-Type", "Authorization", "Bearer", "refreshToken")
             .allowCredentials(true);
